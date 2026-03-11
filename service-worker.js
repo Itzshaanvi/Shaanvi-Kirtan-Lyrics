c2a704e9dd1ade0621e7a3ed6542482d812f2721
Itzshaanvi <69830347+Itzshaanvi@users.noreply.github.com>
2026-03-12 10:10:02 +1300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'kirtan-lyrics-cache-v1';
+
+const STATIC_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',
+  './script.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// INSTALL
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// ACTIVATE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request).then(networkResponse => {
+
+        if (event.request.method === 'GET') {
+
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+
+        }
+
+        return networkResponse;
+
+      }).catch(() => caches.match('./'));
+
+    })
+  );
+});
+
